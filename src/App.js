4006a2c1064ffcbb409bd95e359f2d42ca9d5536
max <promitms7@gmail.com>
2022-11-25 12:54:00 +0600

    carousel and navber done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { RouterProvider } from "react-router-dom";
+import { router } from './pages/Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      Hi tailwind and daisy added
+    <div className="max-w-[1240px] mx-auto">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/pages/Home/Carousel/Carousel.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Carousel = () => {
+  return (
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src="https://i.ibb.co/9yS6Snf/motorcycle-in-price-banner2.jpg"
+          className="w-full"
+          alt="banner"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img
+          src="https://i.ibb.co/djTNk7j/purple-ducati-monster-s4r-motorcycle-website-header.jpg"
+          className="w-full"
+          alt="banner"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
